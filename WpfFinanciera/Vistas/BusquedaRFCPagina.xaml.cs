924d5df3fefd811ff6d2af9b1abfde19df0ce8ec
engaherr <kikgamboa@gmail.com>
2024-03-14 18:21:39 -0600

    feat(vistas y diccionario): IU de busqueda de cliente por RFC añadida con diccionario

---
+++ WpfFinanciera/Vistas/BusquedaRFCPagina.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFinanciera.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para BusquedaRFCCliente.xaml
+    /// </summary>
+    public partial class BusquedaRFCCliente : Page
+    {
+        public BusquedaRFCCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void ClicBuscarCliente(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClicSolicitarCredito(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClicActualizarDatos(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
