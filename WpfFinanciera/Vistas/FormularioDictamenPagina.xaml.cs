9ba2df29aadd6d4500728c924c5dfa4c11beb126
JJacobMS <montielsalasjesus@gmail.com>
2024-03-19 17:27:30 -0600

    feat (Formulario Dictamen) : Se agregan las pantallas del Caso de Uso 05 Evaluar Dictamen

---
+++ WpfFinanciera/Vistas/FormularioDictamenPagina.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfFinanciera.ServicioFinancieraIndependiente;
+
+namespace WpfFinanciera.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para FormularioDictamenPagina.xaml
+    /// </summary>
+    public partial class FormularioDictamenPagina : Page
+    {
+        public FormularioDictamenPagina()
+        {
+            InitializeComponent();
+        }
+
+        private void ClicAceptar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClicCancelar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            Politica politica = checkBox.CommandParameter as Politica;
+
+        }
+
+        private void Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
