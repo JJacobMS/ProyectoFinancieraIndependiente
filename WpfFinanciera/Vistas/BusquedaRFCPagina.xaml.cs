2c5322438526a44753c4dc381922d6f42afef214
engaherr <kikgamboa@gmail.com>
2024-03-19 22:33:06 -0600

    feat(Vistas y Diccionarios): Validaciones para BusquedaRFCPagina añadidas

--- WpfFinanciera/Vistas/BusquedaRFCPagina.xaml.cs
+++ WpfFinanciera/Vistas/BusquedaRFCPagina.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfFinanciera.Utilidades;
 
 namespace WpfFinanciera.Vistas
 {
@@ -27,7 +29,14 @@ namespace WpfFinanciera.Vistas
 
         private void ClicBuscarCliente(object sender, RoutedEventArgs e)
         {
+            string rfc = txtBoxRfc.Text.Trim();
 
+            bool esValido = ValidarRFC(rfc);
+
+            if (esValido)
+            {
+
+            }
         }
 
         private void ClicSolicitarCredito(object sender, RoutedEventArgs e)
@@ -38,6 +47,55 @@ namespace WpfFinanciera.Vistas
         private void ClicActualizarDatos(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void CambioTextoRfc(object sender, TextChangedEventArgs e)
+        {
+            if (txtBoxRfc.Text.Length == 13)
+            {
+                btnBuscar.IsEnabled = true;
+            }
+            else
+            {
+                btnBuscar.IsEnabled = false;
+            }
+        }
+
+        private void PrevenirTextoNoAlfanumerico(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^a-zA-Z0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private bool ValidarRFC(string rfc)
+        {
+            bool esValido = true;
+            string patronRFC = @"^[a-zA-Z]{4}[0-9]{6}[a-zA-Z0-9]{3}$";
+            string razones = "";
+
+            if (string.IsNullOrEmpty(txtBoxRfc.Text.Trim()))
+            {
+                esValido = false;
+                razones = "RFC";
+            } 
+            else
+            {
+                esValido = Regex.IsMatch(rfc, patronRFC);
+                razones = "RFC. Recuerda que el formato es ABCD123456 + Homoclave";
+            }
+
+            if(!esValido)
+            {
+                MostrarVentanaCamposNoValidos(razones);
+            }
+
+            return esValido;
+        }
+
+        private void MostrarVentanaCamposNoValidos(string razones)
+        {
+            VentanaMensaje camposIncorrectos = new VentanaMensaje("Los campos ingresados no son válidos", razones);
+            camposIncorrectos.Mostrar();
         }
     }
 }
