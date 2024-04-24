70aba0efa1c06ecd328a55e95e194b29a6d31bdb
engaherr <kikgamboa@gmail.com>
2024-04-23 19:53:39 -0600

    feat(Vistas y Recursos): añadida GUI FormularioUsuarioPagina e imagenes para mostrar contraseña

---
+++ WpfFinanciera/Vistas/FormularioUsuarioPagina.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFinanciera.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para FormularioUsuarioPagina.xaml
+    /// </summary>
+    public partial class FormularioUsuarioPagina : Page
+    {
+        public FormularioUsuarioPagina()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarPagina(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ClicRegresar(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SeleccionarTipoUsuario(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ClicRegistrar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VerificarCambioTexto(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void VerContraseña(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
