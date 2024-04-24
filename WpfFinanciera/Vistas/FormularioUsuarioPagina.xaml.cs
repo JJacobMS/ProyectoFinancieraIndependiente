f22af3fb34ada3dee4dfffbff2b81b2b07729cc9
engaherr <kikgamboa@gmail.com>
2024-04-24 11:41:34 -0600

    fix(Vistas): Manejo de excepciones y lógica de registro añadid@s

--- WpfFinanciera/Vistas/FormularioUsuarioPagina.xaml.cs
+++ WpfFinanciera/Vistas/FormularioUsuarioPagina.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.ServiceModel;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfFinanciera.ServicioFinancieraIndependiente;
 using WpfFinanciera.Utilidades;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace WpfFinanciera.Vistas
 {
@@ -28,6 +32,7 @@ namespace WpfFinanciera.Vistas
         string _contraseña;
         string _confirmarContraseña;
         bool _contraseñasIguales;
+        int _idTipoUsuarioSeleccionado;
 
         public FormularioUsuarioPagina()
         {
@@ -41,20 +46,37 @@ namespace WpfFinanciera.Vistas
 
         private void ObtenerTiposUsuario()
         {
-            TipoUsuarioClient tipoUsuarioClient = new TipoUsuarioClient();
-            var respuesta = tipoUsuarioClient.RecuperarTiposUsuario();
-            var (tiposUsuario, codigo) = respuesta;
+            Codigo codigoRespuesta = new Codigo();
+            TipoUsuario[] listaTiposUsuario = new TipoUsuario[0];
 
-            switch(codigo)
+            try
+            {
+                TipoUsuarioClient tipoUsuarioClient = new TipoUsuarioClient();
+                var respuesta = tipoUsuarioClient.RecuperarTiposUsuario();
+                var (tiposUsuario, codigo) = respuesta;
+
+                codigoRespuesta = codigo;
+                listaTiposUsuario = tiposUsuario;
+            }
+            catch (CommunicationException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+            catch (TimeoutException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+
+            switch (codigoRespuesta)
             {
                 case Codigo.EXITO:
-                    if(tiposUsuario != null)
+                    if(listaTiposUsuario != null)
                     {
-                        int lastIndex = tiposUsuario.Length - 1;
+                        int lastIndex = listaTiposUsuario.Length - 1;
                         TipoUsuario[] nuevosTiposUsuario = new TipoUsuario[lastIndex];
-                        Array.Copy(tiposUsuario, nuevosTiposUsuario, lastIndex);
-                        tiposUsuario = nuevosTiposUsuario;
-                        lstBoxTiposUsuario.ItemsSource = tiposUsuario;
+                        Array.Copy(listaTiposUsuario, nuevosTiposUsuario, lastIndex);
+                        listaTiposUsuario = nuevosTiposUsuario;
+                        lstBoxTiposUsuario.ItemsSource = listaTiposUsuario;
                     }
                     else
                     {
@@ -64,25 +86,27 @@ namespace WpfFinanciera.Vistas
                     break;
                 case Codigo.ERROR_SERVIDOR:
                     MostrarVentanaErrorServidor();
+                    RegresarAMenuPrincipal();
                     break;
                 case Codigo.ERROR_BD:
                     MostrarVentanaErrorBD();
+                    RegresarAMenuPrincipal();
                     break;
             }
-        }
-
-        private void MostrarVentanaErrorBD()
-        {
-            VentanaMensaje errorBaseDatos = new VentanaMensaje(
-               "Error. No se pudo conectar con el servidor. Inténtelo de nuevo o hágalo más tarde", Mensaje.ERROR);
-            errorBaseDatos.Mostrar();
         }
 
         private void MostrarVentanaErrorServidor()
         {
             VentanaMensaje errorServidor = new VentanaMensaje(
-                "Error. No se pudo conectar con la base de datos. Inténtelo de nuevo o hágalo más tarde", Mensaje.ERROR);
+               "Error. No se pudo conectar con el servidor. Inténtelo de nuevo o hágalo más tarde", Mensaje.ERROR);
             errorServidor.Mostrar();
+        }
+
+        private void MostrarVentanaErrorBD()
+        {
+            VentanaMensaje errorBD = new VentanaMensaje(
+                "Error. No se pudo conectar con la base de datos. Inténtelo de nuevo o hágalo más tarde", Mensaje.ERROR);
+            errorBD.Mostrar();
         }
 
         private void MostrarMensajeTiposUsuarios()
@@ -92,6 +116,11 @@ namespace WpfFinanciera.Vistas
         }
 
         private void ClicRegresar(object sender, MouseButtonEventArgs e)
+        {
+            RegresarAMenuPrincipal();
+        }
+
+        private void RegresarAMenuPrincipal()
         {
             MenuPrincipalAdministradorPagina menuPrincipal = new MenuPrincipalAdministradorPagina();
 
@@ -116,6 +145,11 @@ namespace WpfFinanciera.Vistas
             _contraseña = string.IsNullOrEmpty(pswBoxContraseña.Password.Trim()) ? txtBoxMostrarContraseña.Text : pswBoxContraseña.Password.Trim();
             _confirmarContraseña = string.IsNullOrEmpty(pswBoxContraseñaRepetida.Password.Trim()) ? txtBoxMostrarContraseñaRepetida.Text : pswBoxContraseñaRepetida.Password.Trim();
             _contraseñasIguales = _contraseña == _confirmarContraseña;
+
+            if(lstBoxTiposUsuario.SelectedIndex != -1)
+            {
+                _idTipoUsuarioSeleccionado = (lstBoxTiposUsuario.SelectedItem as TipoUsuario).idTipoUsuario;
+            }
         }
 
         private void HabilitarRegistrar()
@@ -125,8 +159,171 @@ namespace WpfFinanciera.Vistas
 
         private void ClicRegistrar(object sender, RoutedEventArgs e)
         {
+            string motivos;
 
+            motivos = ValidarCorreo();
+            motivos += ValidarContraseñaSegura();
+
+            if(motivos != "")
+            {
+                MostrarVentanaError(motivos);
+            }
+            else
+            {
+                MostrarVentanaConfirmacion();
+            }
         }
+
+        private void MostrarVentanaConfirmacion()
+        {
+            VentanaMensaje ventanaConfirmacion = new VentanaMensaje("¿Desea registrar el usuario ingresado?", Mensaje.CONFIRMACION);
+            if(ventanaConfirmacion.MostrarConfirmacion())
+            {
+                RegistrarUsuario();
+            }
+        }
+
+        private void RegistrarUsuario()
+        {
+            Codigo codigoRespuesta = new Codigo();
+            bool correoUnico = false;
+
+            try
+            {
+                UsuarioClient usuarioClient = new UsuarioClient();
+                var respuesta = usuarioClient.EsCorreoUnico(_correo);
+                var (codigo, esCorreoUnico) = respuesta;
+
+                codigoRespuesta = codigo;
+                correoUnico = esCorreoUnico;
+            }
+            catch (CommunicationException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+            catch (TimeoutException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+
+            switch(codigoRespuesta)
+            {
+                case Codigo.EXITO:
+                    if(correoUnico)
+                    {
+                        RegistrarUsuarioBD();
+                    }
+                    else
+                    {
+                        MostrarVentanaError("Correo ya registrado.");
+                    }
+                    break;
+                case Codigo.ERROR_SERVIDOR:
+                    MostrarVentanaErrorServidor();
+                    break;
+                case Codigo.ERROR_BD:
+                    MostrarVentanaErrorBD();
+                    break;
+            }
+        }
+
+        private void RegistrarUsuarioBD()
+        {
+            Console.WriteLine(_idTipoUsuarioSeleccionado);
+
+            Usuario usuarioNuevo = new Usuario
+            {
+                nombres = _nombres,
+                apellidos = _apellidos,
+                correoElectronico = _correo,
+                contrasenha = _contraseña,
+                TipoUsuario_idTipoUsuario = _idTipoUsuarioSeleccionado
+            };
+
+            Codigo codigoRespuesta = new Codigo();
+
+            try
+            {
+                UsuarioClient usuarioClient = new UsuarioClient();
+                var codigo = usuarioClient.GuardarUsuario(usuarioNuevo);
+
+                codigoRespuesta = codigo;
+            }
+            catch (CommunicationException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+            catch (TimeoutException)
+            {
+                codigoRespuesta = Codigo.ERROR_SERVIDOR;
+            }
+
+            switch (codigoRespuesta)
+            {
+                case Codigo.EXITO:
+                    MostrarVentanaExito();
+                    RegresarAMenuPrincipal();
+                    break;
+                case Codigo.ERROR_SERVIDOR:
+                    MostrarVentanaErrorServidor();
+                    break;
+                case Codigo.ERROR_BD:
+                    MostrarVentanaErrorBD();
+                    break;
+            }
+        }
+
+        private void MostrarVentanaExito()
+        {
+            VentanaMensaje exito = new VentanaMensaje("Se ha registrado el usuario exitosamente", Mensaje.EXITO);
+            exito.Mostrar();
+        }
+
+        private void MostrarVentanaError(string motivos)
+        {
+            VentanaMensaje error = new VentanaMensaje("Los campos ingresados no son válidos", motivos);
+            error.Mostrar();
+        }
+
+        private string ValidarCorreo()
+        {
+            string motivos;
+
+            try
+            {
+                _ = new MailAddress(_correo);
+                motivos = "";
+            }
+            catch (FormatException)
+            {
+                motivos = "Correo con formato no válido.";
+            }
+
+            return motivos;
+        }
+
+        private string ValidarContraseñaSegura()
+        {
+            string motivos = "";
+            string expresion = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*?#\-&_]{8,}$";
+
+            TimeSpan timeout = TimeSpan.FromMilliseconds(500);
+
+            try
+            {
+                if (!Regex.IsMatch(_contraseña, expresion, RegexOptions.None, timeout))
+                {
+                    motivos = "Contraseña débil, ingresa una contraseña que contenga al menos una mayúscula, una minúscula y un número e intenta de nuevo.";
+                }
+            }
+            catch (RegexMatchTimeoutException ex)
+            {
+                motivos = "Error al validar la contraseña.";
+            }
+
+            return motivos;
+        }
+
 
         private void VerificarCambioTexto(object sender, TextChangedEventArgs e)
         {
@@ -138,9 +335,9 @@ namespace WpfFinanciera.Vistas
                 string nombreTextBox = textBox.Name;
 
                 if ((nombreTextBox == "txtBoxNombre" || nombreTextBox == "txtBoxApellidos") &&
-                    !System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "^[a-zA-Z0-9]+$"))
+                    !Regex.IsMatch(textBox.Text, "^[a-zA-Z0-9]+$"))
                 {
-                    textBox.Text = string.Join("", textBox.Text.Where(char.IsLetterOrDigit));
+                    textBox.Text = string.Join("", textBox.Text.Where(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)));
                 }
 
                 if (textBox.Text.Trim() == "")
